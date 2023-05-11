b931b86e91d64d89729670f827a03e5641da05dc
saeed-alrafi <saeed.alrafi@hotmail.com>
2023-05-11 15:36:52 +0400

    object-manager PoC done

---
+++ obm/app.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const app = new Koa();
+var objectMapper = require('object-mapper');
+
+app.use(async ctx => {
+  const fatiha = await fetch("https://api.alquran.cloud/v1/surah/1");
+  const fatihaJson = await fatiha.json()
+  var map = {
+    "data.name":"name",
+    "data.numberOfAyahs":"numberOfAyahs",
+    "data.ayahs[2]":"3rd ayah "
+  }
+  var x = objectMapper(fatihaJson,map);
+  ctx.body = x;
+});
+
+app.listen(3000);
